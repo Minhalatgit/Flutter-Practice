11dae139d51a15bac65cbfeba0de0357568d8b8e
Minhal Nadeem <minhal@tekrevol.com>
2023-03-05 20:21:10 +0500

    Project setup

---
+++ android/app/src/main/kotlin/com/example/flutterpractice/flutter_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterpractice.flutter_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<String> _topics = ["Row & Column", "Container"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: ListView.separated(
+        itemCount: _topics.length,
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        separatorBuilder: (context, index) => const Divider(
+          height: 50,
+        ),
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: (){
+            _onTapTopic(index);
+          },
+          child: Text(
+            _topics[index],
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onTapTopic(int index){
+    switch(index){
+      case 1:{
+
+        break;
+      }
+    }
+  }
+}
